7552e9295ad4dc2043ff98e6f754502ff1bcc698
Ayesha <shaikayesha95155@gmail.com>
2023-03-09 16:49:06 +0530

    integrated with json server

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,16 +2,39 @@
   <img class="logo" src="../assets/restaurant_logo.png" />
   <h1>Sign UP</h1>
   <div class="register">
-    <input type="text" placeholder="Enter Name" />
-    <input type="text" placeholder="Enter Email" />
-    <input type="Password" placeholder="Enter Password" />
-    <button>Sign Up</button>
+    <input type="text" v-model="name" placeholder="Enter Name" />
+    <input type="text" v-model="email" placeholder="Enter Email" />
+    <input type="Password" v-model="password" placeholder="Enter Password" />
+    <button v-on:click="signUp">Sign Up</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signUp() {
+      console.warn("signup", this.name, this.email, this.password);
+      let result = await axios.post("http://localhost:3000/users", {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      });
+      console.warn(result);
+      if (result.status == 201) {
+        alert("sign up done");
+      }
+      localStorage.setItem("user-info", JSON.stringify(result.data));
+    },
+  },
 };
 </script>
 <style>
